f26ffb1acb9db59d75ed85c83981b93c211fcb22
Saravanan-Py-Dev <115641098+Saravanan-Py-Dev@users.noreply.github.com>
2023-04-16 11:36:04 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
-from kivy.uix.boxlayout import BoxLayout
+from kivy.core.window import Window
+from kivy.uix.screenmanager import ScreenManager
 from kivymd.app import MDApp
 
-from lib.python import UI
+from lib.python import UI, WindowManger
+
+Window.size = (360, 600)
+
+# move textinput under the keyboard in above the keyboard
+Window.keyboard_anim_args = {'d': .2, 't': 'in_out_expo'}
+Window.softinput_mode = "below_target"
+
 
 
 class MyApp(MDApp):
     def build(self):
+        self.theme_cls.primary_palette = "Purple"
+        Window. softinput_mde = "below target"
         self.load_all_kv_files("lib/")
-        return UI()
+        return WindowManger()
 
 
 if __name__ == '__main__':
